f2b08d9b495949d1e3464d995511c032be4de5cb
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-06-05 12:32:58 +0430

    add lists component to the app

--- src/app/modules/main/basic-info/basic-info-routing.module.ts
+++ src/app/modules/main/basic-info/basic-info-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompaniesPayListComponent } from './companies-pay-list/companies-pay-list.component';
+import { ContractListComponent } from './contract-list/contract-list.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { RegisterNewContractComponent } from './register-new-contract/register-new-contract.component';
 import { RegisterNewRepresentativeContractComponent } from './register-new-representative-contract/register-new-representative-contract.component';
 import { RegisterProjectInfoComponent } from './register-project-info/register-project-info.component';
 import { RepresentativeContractsListComponent } from './representative-contracts-list/representative-contracts-list.component';
@@ -16,7 +20,24 @@ const routes: Routes = [
     {
       path: 'MarketerContractList' ,
       component: RepresentativeContractsListComponent
+    },
+    {
+      path: 'addNewContract' ,
+      component: RegisterNewContractComponent
+    },
+    {
+      path: 'ContractList' ,
+      component: ContractListComponent
+    },
+    {
+      path: 'PspList' ,
+      component: CompaniesPayListComponent
+    },
+    {
+      path: 'ProjectList' ,
+      component: ProjectListComponent
     }
+
 ];
 
 @NgModule({
