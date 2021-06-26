21b710c6252bac83044e2309170e960602654ad1
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-06-26 14:37:02 +0430

    change the layout

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,14 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  currentDay;
   constructor() { }
+  
 
   ngOnInit(): void {
+    this.displayToday(); 
   }
 
   logOut(){
     localStorage.removeItem('token');
     window.location.href = '/';
   }
+
+  displayToday() {
+    const faDateTime = new Intl.DateTimeFormat("fa", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format;
+    const now = Date.now();
+    this.currentDay = faDateTime(now);
+  }
+
+
 
 }
