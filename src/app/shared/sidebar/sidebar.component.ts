777d47947c6a038eb50b2effd2a41934a8a09a3c
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-06-27 23:45:07 +0430

    make dashboard page 7

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+ openById = {};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  test(event){
+    this.openById[event.panelId] = event.nextState;
   }
 
 }
